573af681731ced6ea9775eb2500a22be7494101b
Abhinandan Gupta <abhig0245@gmail.com>
2025-11-16 21:04:49 +0530

    create listing api routes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 // import cors from "cors"; // optional if you add it later
 
@@ -29,6 +30,7 @@ mongoose
 // ✅ Mount routes BEFORE listen
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 // ✅ Global error handler
 app.use((err, req, res, next) => {
